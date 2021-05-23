b3e5a9174bd2dbd124a8390265431826b351d222
Rakhesh Sasidharan <me@rakhesh.com>
2021-05-23 13:26:29 +0100

    adding azure bits

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for var in `env`; do
-    echo ">> $var" >> /usr/share/nginx/html/vars.txt
+    echo ">> $var" >> /usr/share/nginx/html/more/vars.txt
 done
-echo "=========================" >> /usr/share/nginx/html/vars.txt
+
+echo "=========================" >> /usr/share/nginx/html/more/vars.txt
 nginx -g "daemon off;"
