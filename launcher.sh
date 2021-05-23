0126cd77e4d883cda50b0be63cb983d8911056b4
Rakhesh Sasidharan <me@rakhesh.com>
2021-05-23 14:04:22 +0100

    file rename

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 if [ ! -d /usr/share/nginx/html/more ]; then mkdir /usr/share/nginx/html/more; fi
+
 for var in `env`; do
-    echo ">> $var" >> /usr/share/nginx/html/more/vars.txt
+    echo ">> $var" >> /usr/share/nginx/html/more/env.txt
 done
 
-echo "=========================" >> /usr/share/nginx/html/more/vars.txt
+echo "=========================" >> /usr/share/nginx/html/more/env.txt
 nginx -g "daemon off;"
